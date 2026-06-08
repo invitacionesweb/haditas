3a0886423ffe4f99cb71ef6e47a023b070ca2839
invitacionesweb <invitacionesweb.info@gmail.com>
2026-06-07 22:42:25 -0300

    a
    a

---
+++ config.js
@@ -0,0 +1,45 @@
+// =============================================
+//  CONFIGURACIÓN DE LA INVITACIÓN - ROMA
+//  Editá solo este archivo para cada cliente
+// =============================================
+
+const CONFIG = {
+
+  // --- DATOS DEL EVENTO ---
+  nombreFestejado: "Martina",
+  subtitulo: "Te esperamos",
+
+  // --- FECHA PARA EL CONTADOR (ISO 8601) ---
+  fechaContador: "2026-08-28T23:59:59",
+
+  // --- UBICACIÓN ---
+  // --- WHATSAPP ---
+  linkWhatsapp: "https://wa.link/cpiym5",
+
+  textoMensaje: "Mi primer añito se acerca y cómo sos parte de mi historia quiero festejarlo con vos.",
+  textoUbicacion: "Te espero a las 14:00 hs en Jano's eventos - Hace clic para saber como llegar.",
+  linkMapa: "https://www.google.com/maps/dir//Janos+San+Telmo,+Per%C3%BA+338,+C1063+Cdad.+Aut%C3%B3noma+de+Buenos+Aires/@-34.6536719,-58.4253789,13z/data=!4m18!1m8!3m7!1s0x95bccbce7c872ecb:0xc1513587688ba57e!2sJanos+San+Telmo!8m2!3d-34.6118796!4d-58.3747124!15sCgVqYW5vc1oHIgVqYW5vc5IBFmZ1bmN0aW9uX3Jvb21fZmFjaWxpdHmaASRDaGREU1VoTk1HOW5TMFZKUTBGblNVTnlaMTlxVVhSM1JSQULgAQD6AQQIABBI!16s%2Fg%2F11lh3vygjj!4m8!1m0!1m5!1m1!1s0x95bccbce7c872ecb:0xc1513587688ba57e!2m2!1d-58.3747124!2d-34.6118796!3e0?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D",
+  linkAgendar: "",
+
+  // --- FORMULARIO ---
+  textoWhatsappT: "Sólo faltas vos, ¿venis?",
+  textoWhatsappP:  "No olvides confirmar asistencia con mis papis antes del 01 de Julio.",
+  textoForm: "Sólo faltas vos, ¿venis?",
+  textoFormSub: "No olvides confirmar asistencia con mis papis antes del 01 de Julio.",
+  maxInvitados: 5,
+  scriptFormUrl: "https://script.google.com/macros/s/AKfycbzvOHJX4vf1j7dECzp9wkJv9TZzXi3egD9esUMqWlTbCMFoqTvqY-iMHwVwNhx1HITC/exec",
+
+  // --- AUDIO ---
+  archivoAudio: "./FCMELODICO.mp3",
+
+  // --- COLORES ---
+  colores: {
+    color0: "#ffffff",
+    color1: "#E6C5BC",
+    color2: "#f8f3ed",
+    color3: "#A6B7A1",
+  },
+
+  // --- TÍTULO DE LA PESTAÑA ---
+  tituloTab: "Martina",
+};
